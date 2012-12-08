38945e3d3cc1792aec4aac68d2ff780fe7425dc9
Joshua Abbott <joshua.abbott@berkeley.edu>
2012-12-07 22:30:09 -0800

    fixed bug in hopfield code: needed to initialize data to a copy of address

--- hopfield-scrap.py
+++ hopfield-scrap.py
@@ -4,9 +4,9 @@ from util import corrupt, plot_io
 import hopfield as hopfield
 
 def like_matlab(arr):
-	n = int(np.sqrt(arr.shape[0]))
-	newarr = arr[:, 0].reshape((n, n), order='F').ravel()[:, None]
-	return newarr
+    n = int(np.sqrt(arr.shape[0]))
+    newarr = arr[:, 0].reshape((n, n), order='F').ravel()[:, None]
+    return newarr
 
 data = np.load('patterns.npz')
 X = like_matlab(data['X'])
@@ -17,8 +17,9 @@ num1 = like_matlab(np.load('1.npy'))
 num2 = like_matlab(np.load('2.npy'))
 num3 = like_matlab(np.load('3.npy'))
 num4 = like_matlab(np.load('4.npy'))
-inputs = np.hstack([face, X, hi, num1, num2, num3, num4])
+#inputs = np.hstack([face, X, hi, num1, num2, num3, num4])
 
+inputs = np.hstack([face, X, hi, num1, num2]).astype('f8')
 
 hop = hopfield.hopnet(inputs)
 addresses = inputs.copy()
@@ -26,8 +27,23 @@ numNeurons,numPatterns = addresses.shape
 
 numIters = 1000
 
+
+
+print "uncorrupted tests"
 for i in xrange(numPatterns):
-	a = corrupt(addresses[:, i], 10)
-	d = hop.read(numIters,a).reshape((10, 10), order='F')
-	plt.figure(i)
-	plot_io(a.reshape((10, 10), order='F'), d)
+    a = addresses[:, i]
+    d = hop.read(numIters,a).reshape((10, 10), order='F')
+    plt.figure(i)
+    plot_io(a.reshape((10, 10), order='F'), d)
+
+
+
+
+
+print "now with corruption"
+
+for i in xrange(numPatterns):
+    a = corrupt(addresses[:, i], 10)
+    d = hop.read(numIters,a).reshape((10, 10), order='F')
+    plt.figure(i+numPatterns)
+    plot_io(a.reshape((10, 10), order='F'), d)
--- hopfield.py
+++ hopfield.py
@@ -1,66 +1,59 @@
+"""hopfield.py -- Hopfield Network
+
+Authors:
+  Jessica Hamrick (jhamrick@berkeley.edu)
+  Josh Abbott (joshua.abbott@berkeley.edu)
+
+"""
+
 import numpy as np
-import matplotlib.pyplot as plt
 
 class hopnet(object):
 
-	def __init__(self,input=None):
-		"""Initialize a Hopfield network
+    def __init__(self,input=None):
+        """Initialize a Hopfield network
 
-		Parameters
-		----------
-		input	: an hstack of the input		
+        Parameters
+        ----------
+        input	: an hstack of the input		
 
-		"""
+        """
 
-		# save parameters
-		self.numNeurons,self.numPatterns = input.shape
+        # save parameters
+        self.numNeurons,self.numPatterns = input.shape
 
-		# create connectivity matrix
-		self.T = np.zeros((self.numNeurons,self.numNeurons))
+        # create connectivity matrix
+        self.T = np.zeros((self.numNeurons,self.numNeurons))
 		
-		# initialize T using hopfield init rule
-		for i in xrange(self.numPatterns):
-			self.T += np.outer(input[:,i],input[:,i])
+        # initialize T using hopfield init rule
+        for i in xrange(self.numPatterns):
+            self.T += np.outer(input[:,i],input[:,i])
 
-		self.T /= self.numPatterns
-
-	def read(self, iters=None, address=None):
-		"""Simulates a Hopfield network
-
-		Parameters
-		----------
-		iters : int 
-			number of iterations to run
-		address : array or None
-			initial input for the network (numNeuronsx1) to test
-
-		Returns
-		-------
-		data : array
-			the result of running `iters` iterations of the hopfield network 
-			defined by the matrix `T`, starting with input `address`
-		"""
-
-		sz=np.sqrt(self.numNeurons);
-
-		data = address
-
-#		idx = np.random.randint(self.numNeurons, size=iters)
-#		data[idx]= 2*(np.dot(self.T[idx,:],data)>0) -1
-		
-
-#		h=plt.imshow(data.reshape((sz,sz)).T,vmin=-1, vmax=1, interpolation='nearest');
-#		fig, ax = plt.gcf(),plt.gca()
-#		plt.draw()
-
-		for i in np.random.randint(self.numNeurons, size=iters): # pick the neuron to flip
-			# compute net input and flip state accordingly
-			data[i]= 2*(np.dot(self.T[i,:],data)>0) -1
-
-			# refresh display
-#			h.set_data(data.reshape((sz,sz)).T)
-#			plt.draw()
+        self.T /= self.numPatterns
 
 
-		return data
+    def read(self, iters=None, address=None):
+        """Simulates a Hopfield network
+
+        Parameters
+        ----------
+        iters : int 
+            number of iterations to run
+        address : array or None
+            initial input for the network (numNeuronsx1) to test
+
+        Returns
+        -------
+        data : array
+            the result of running `iters` iterations of the hopfield network 
+            defined by the matrix `T`, starting with input `address`
+        """
+
+        data = address.copy()
+
+        idx = np.random.randint(self.numNeurons, size=iters)
+        data[idx]= 2*(np.dot(self.T[idx,:],data)>0) -1
+
+
+        return data
 
